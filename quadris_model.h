99f634417d140d1bc4b3c64f13e97e0c2ea40271
Ryan Quanz <ryan.quanz@shopify.com>
2016-11-19 19:15:32 -0500

    add model header

---
+++ quadris_model.h
@@ -0,0 +1,27 @@
+#ifndef _QUADRIS_MODEL_H_
+#define _QUADRIS_MODEL_H_
+#include "quadris_model.h"
+#include "block.h"
+
+class QuadrisModel {
+public:
+  QuadrisModel();
+  void down();
+  void right();
+  void left();
+  void drop();
+  void levelUp();
+  void levelDown();
+  bool isOver();
+
+private:
+  void clearRow();
+  std::vector<Block&> blocks;
+  int level;
+  static int high_score;
+  int score;
+  int seed;
+  std::string sequence_file;
+}
+
+#endif
